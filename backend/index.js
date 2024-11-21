7b885c33d0ae6260dc8a68f0cf7840055becaa6a
Manjil246 <manjildhungana8@gmail.com>
2024-11-21 23:32:48 +0530

    Push3

--- backend/index.js
+++ backend/index.js
@@ -1,36 +1,33 @@
-import {connectToMongo} from "./db.js";
+import { connectToMongo } from "./db.js";
 import express from "express";
-import user from "./routes/user.js"
-import post from "./routes/post.js"
-import cookieParser from "cookie-parser"
+import user from "./routes/user.js";
+import post from "./routes/post.js";
+import cookieParser from "cookie-parser";
 const app = express();
-import dotenv from "dotenv"
+import dotenv from "dotenv";
 dotenv.config();
-import cloudinary from "cloudinary"
+import cloudinary from "cloudinary";
 
 cloudinary.config({
-    cloud_name : process.env.CLOUDINARY_NAME,
-    api_key : process.env.CLOUDINARY_API_KEY,
-    api_secret : process.env.CLOUDINARY_API_SECRET
-})
+  cloud_name: process.env.CLOUDINARY_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 
 app.set("trust proxy", 1);
 
-import cors from "cors"
+import cors from "cors";
 app.use(
   cors({
-    origin: "social-media-app-g7fd.vercel.app",
+    origin: "https://social-media-app-g7fd.vercel.app",
     credentials: true,
   })
 );
 
-app.options('*',cors());
+app.options("*", cors());
 
-
-
-app.use(express.json({limit:'50mb'}))
-app.use(cookieParser())
-
+app.use(express.json({ limit: "50mb" }));
+app.use(cookieParser());
 
 const port = process.env.PORT;
 
@@ -40,13 +37,10 @@ connectToMongo();
 //     res.send("Hello World")
 // })
 
-
 //Routes
-app.use("/user",user)
-app.use("/post",post)
+app.use("/user", user);
+app.use("/post", post);
 
-
-app.listen(port,()=>{
-    console.log(`Social media app running at http://localhost:${port}`)
-})
-
+app.listen(port, () => {
+  console.log(`Social media app running at http://localhost:${port}`);
+});
